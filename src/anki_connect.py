8642095fbab8499fe6dc5043e225815f23f62566
silveraMatu <matusilvera808@gmail.com>
2026-02-14 03:32:53 -0300

    Func add_note now can sends requests to ankiConnect to save new cards

--- src/anki_connect.py
+++ src/anki_connect.py
@@ -1,10 +1,35 @@
-# import requests
+import requests
 import json
 
 with open("./src/config.json", "r") as file:
   config = json.load(file)
 
-def add_note(front, back, dec):
-  pass
+def add_note(front, back, deck=config.get("DECK_NAME")):
+  '''
+  It sends a post request to Ankiconnect to create a card
+  '''
 
-print(config)
+  payload = {
+    "action": "addNote",
+    "version": "6",
+    "params": {
+      "note": {
+        "deckName": deck,
+        "modelName": "Basic",
+        "fields":{
+          "Front": front,
+          "Back": back
+        },
+        "tags": ["cli_added", "english"]
+      }
+    }
+  }
+  try:
+    response = requests.post(config.get("ANKI_URL"), json=payload)
+    result = response.json()
+
+    if result.get("error"):
+      return False, f"Anki intern error: {result["error"]}"
+    return True, f"Card sent with id: {result["result"]}"
+  except requests.exceptions.ConnectionError:
+    return False, "Cannot connect with Anki. It's open?"
